1c7385c07faf533ef2d3a9892c2cf66e16b1728f
MarciovsRocha <dev.marcio.rocha@gmail.com>
2020-03-05 21:45:20 -0300

    criado repositorio da disciplina de descoberta e evolucao da computacao

---
+++ aula_03-03-2020/crypto.py
@@ -0,0 +1,70 @@
+lista = ['a', 'b', 1, 'd']
+index = lista.index(1)
+# valores correspondentes de index do array 1  - 13 correspondem a sequencia de letras a - m
+# valores correspondentes de index do array 14 - 25 correspondem a sequencia de letras z - n
+# lista = lista de valores codificados
+
+def deCode():
+    if lista.index('D2G7H8') == 0:
+        return 'a'
+    elif lista.index('EJ6Y89') == 1:
+        return 'b'
+    elif lista.index('FO2D6F') == 2:
+        return 'c'
+    elif lista.index('GFGR56') == 3:
+        return 'd'
+    elif lista.index('H89KJI') == 4:
+        return 'e'
+    elif lista.index('IJ0OK6') == 5:
+        return 'f'
+    elif lista.index('JOI980') == 6:
+        return 'g'
+    elif lista.index('KGU870') == 7:
+        return 'h'
+    elif lista.index('LRF567') == 8:
+        return 'i'
+    elif lista.index('MYI547') == 9:
+        return 'j'
+    elif lista.index('NHJ098') == 10:
+        return 'k'
+    elif lista.index('OJG123') == 11:
+        return 'l'
+    elif lista.index('PSD876') == 12:
+        return 'm'
+    elif lista.index('QYU543') == 13:
+        return 'n
+    elif lista.index('RNV354') == 14:
+        return 'o'
+    elif lista.index('SJZ748') == 15:
+        return 'p'
+    elif lista.index('THF390') == 16:
+        return 'q'
+    elif lista.index('UFU202') == 17:
+        return 'r'
+    elif lista.index('XYH398') == 18:
+        return 's'
+    elif lista.index('WSK392') == 19:
+        return 't'
+    elif lista.index('XYH398') == 20:
+        return 'u'
+    elif lista.index('YFG289') == 21:
+        return 'v'
+    elif lista.index('ZHE092') == 22:
+        return 'w'
+    elif lista.index('AFN298') == 23:
+        return 'x'
+    elif lista.index('BYD290') == 24:
+        return 'y'
+    elif lista.index('CJD945') == 25:
+        return 'z'
+    elif lista.index('H734VC') == 26:
+        return 'é,É'
+    elif lista.index('D2G7S8') == 27:
+        return 'Ã,ã'
+    elif lista.index('D2G7H0') == 28:
+        return 'À,à'
+    elif lista.index('D2G7H9') == 29:
+        return 'Á,á'
+    elif lista.index('RNV358') == 30:
+        return 'Õ,õ'
+    elif lista.index('
