8548fc79b2611ac6b2033a7d5d45b1b16ec10dcf
MarciovsRocha <dev.marcio.rocha@gmail.com>
2020-04-07 19:57:49 -0300

    Arrumado esquema de Datas e organizacao]

---
+++ 20200407/Atividade_2/atividade2.py
@@ -0,0 +1,8 @@
+num = int(input("Digite um Numero Inteiro: "))
+    if ((num % 2) == 0 ):
+        print(f"O Numero {num} eh par")
+    else:
+        print(f"O Numero {num} eh impar")
+    
+# =====================================================================
+
